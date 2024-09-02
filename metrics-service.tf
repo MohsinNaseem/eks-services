fedfd4cee72bdad4ffdd32d4a03cdc8a859ad4df
MohsinNaseem <perfect_889@hotmail.com>
2024-09-03 00:27:10 +0500

    added helm terraform files

--- metrics-service.tf
+++ metrics-service.tf
@@ -1,5 +1,6 @@
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
+  namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   chart      = "metrics-server"
   version    = "0.6.1"
